c7a9f42dc7ca2f56f825c6457a112a275937b7db
seizekena <1805pjh@naver.com>
2016-06-01 15:21:05 +0900

    시간동기화 함수 테스트
    1.clock_gettime
    
    2.함수 수정
    
    3.sleep 함수 제거 (reverted from commit a26bbc6e158eaf6808dbd6dad6c274acd923e3f0)
    
    4.슬레이브 추가
    
    5.hi의 추가

---
+++ hi.cpp
@@ -0,0 +1,15 @@
+/*
+ * hi.cpp
+ *
+ *  Created on: 2016. 6. 1.
+ *      Author: parallels
+ */
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+void func()
+{
+	printf("나는 hi의 func이야\n");
+}
