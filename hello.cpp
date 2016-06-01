6e7f04427b8e7001f751b07f6e8bf7270945cab9
seizekena <1805pjh@naver.com>
2016-06-01 11:36:33 +0900

    시간동기화 함수 테스트
    1.clock_gettime

---
+++ hello.cpp
@@ -0,0 +1,35 @@
+//============================================================================
+// Name        : hello.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+using namespace std;
+
+int main() {
+	struct timespec sCurNanoTimeVal;
+	struct timespec sCurNanoTimeVal2;
+	int rtn, i;
+
+	for (i = 0; i < 5; i++)
+	{
+		rtn = clock_gettime(CLOCK_REALTIME, &sCurNanoTimeVal);
+		printf("[i:%06d] %ld %ld\n", i, sCurNanoTimeVal.tv_sec,
+				sCurNanoTimeVal.tv_nsec);
+
+		rtn = clock_gettime(CLOCK_BOOTTIME, &sCurNanoTimeVal2);
+		printf("[i:%06d] %ld %ld \n--\n", i, sCurNanoTimeVal2.tv_sec,
+				sCurNanoTimeVal2.tv_nsec);
+
+		sleep(1);
+	}
+
+	return 0;
+}
+
